a60f7fecbd1a20c90ac63096c7860105fa44206c
SYVTT <czaczek19@wp.pl>
2018-06-17 01:32:17 +0200

    Server fixed

--- udp_server.py
+++ udp_server.py
@@ -8,16 +8,13 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
 sock.bind((UDP_IP, UDP_PORT))
 while True:
-    data, _ = sock.recvfrom(8)
+    data, _ = sock.recvfrom(16)
     print("received message:", data)
 
     message = data.decode()
-    if message == 'cl':
+    tab = message.split(' ')
+    # x, y, cl (int, int 0/1)
+    x, y, cl = int(tab[0]), int(tab[1]), int(tab[2])
+    move(x, y)
+    if cl == 1:
         click()
-    else:
-        tab = message.split(' ')
-        # x, y, cl (int, int 0/1)
-        x, y, cl = int(tab[0]), int(tab[1]), int(tab[2])
-        move(x, y)
-        if cl == 1:
-            click()
